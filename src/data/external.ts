63d03d51f26af27f12ba28a37a8444e9eec56d72
mpaulweeks <mpaulweeks@gmail.com>
2023-05-25 12:44:30 -0400

    add data and cleanup placeholder copy

---
+++ src/data/external.ts
@@ -0,0 +1,24 @@
+export type ExternalLink = {
+  label: string;
+  url: string;
+}
+
+export const ExternalLinks: ExternalLink[] = [{
+  label: 'email',
+  url: "mailto:mpaulweeks@gmail.com",
+}, {
+  label: 'twitter',
+  url: "https://twitter.com/mpaulweeks",
+}, {
+  label: 'instagram',
+  url: "https://www.instagram.com/mpaulweeks/",
+}, {
+  label: 'github',
+  url: "https://github.com/mpaulweeks",
+}, {
+  label: 'blog',
+  url: "https://blog.mpaulweeks.com",
+}, {
+  label: 'patreon',
+  url: "https://patreon.com/mpaulweeks",
+}]
