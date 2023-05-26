1ec6af3b9291ac9562ab7048c212b39bc02d27b3
mpaulweeks <mpaulweeks@gmail.com>
2023-05-26 11:28:43 -0500

    projects

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
+import { Projects } from '@/data/projects'
+import { ProjectLink } from '@/comp/ProjectLink'
 
 export default function Home() {
   return (
     <main className={styles.main}>
       Hello world
+      {Projects.map(p => (
+        <ProjectLink key={p.name} info={p} />
+      ))}
     </main>
   )
 }
