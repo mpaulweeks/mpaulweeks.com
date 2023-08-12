f4b530ba7ddc2be34f140d99da9319b9d9aa1450
mpaulweeks <mpaulweeks@gmail.com>
2023-08-12 02:11:06 -0400

    add gtag

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import { Metadata } from 'next'
+import Script from 'next/script'
 import './globals.css'
 import { Inter } from 'next/font/google'
 
@@ -34,6 +35,20 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>{children}</body>
+      <Script>
+        {`
+          var _gaq = _gaq || [];
+          _gaq.push(['_setAccount', 'UA-37989728-1']);
+          _gaq.push(['_setDomainName', 'mpaulweeks.com']);
+          _gaq.push(['_trackPageview']);
+
+          (function() {
+              var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+              ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+              var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+          })();
+        `}
+      </Script>
     </html>
   )
 }
